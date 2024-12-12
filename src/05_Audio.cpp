927d744bf841ae25be26d6f33e38fe1c874520dc
manuel121912 <a23310403@ceti.mx>
2024-12-11 19:36:55 -0600

    03Commit(M)

--- src/05_Audio.cpp
+++ src/05_Audio.cpp
@@ -6,7 +6,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Reproductor de musica");
 
     sf::Music music;
-    if (!music.openFromFile("./assets/music/musica.ogg"))
+    if (!music.openFromFile("assets/music/Cyberpunk-Moonlight-Sonata.ogg"))
     {
         // Error al cargar el archivo de música
         return -1;
